c7e61ce2617a9dc6f96de828cf02e5c0f68bffc1
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-09 22:08:23 +0100

    commit

--- src/main/java/com/example/keycloak/resource/KeycloakResourceServer/KeycloakResourceServerApplication.java
+++ src/main/java/com/example/keycloak/resource/KeycloakResourceServer/KeycloakResourceServerApplication.java
@@ -2,11 +2,13 @@ package com.example.keycloak.resource.KeycloakResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.example.keycloak.resource.KeycloakResourceServer.security")
 @ComponentScan("com.example.keycloak.resource.KeycloakResourceServer.controller")
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class KeycloakResourceServerApplication {
 
